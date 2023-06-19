79b8131ac6fc9e4be905d94a379d1860c545b6a9
aman-ur-rehman-sandhu <engr.amansandhu@outlook.com>
2023-06-19 11:12:22 +0500

    Control the camera with mouse movemnet and Learn about some other Three.js Control.

--- src/script.js
+++ src/script.js
@@ -1,17 +1,6 @@
+import './style.css';
 import * as THREE from 'three';
-import gsap from 'gsap';
-
-// Canvas
-const canvas = document.querySelector('canvas.webgl');
-
-// Scene
-const scene = new THREE.Scene();
-
-// Object
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 // Sizes
 const sizes = {
@@ -19,48 +8,95 @@ const sizes = {
   height: 600,
 };
 
+// Cursor
+const cursor = {
+  x: 0,
+  y: 0,
+};
+
+window.addEventListener('mousemove', (event) => {
+  cursor.x = event.clientX / sizes.width - 0.5;
+  cursor.y = -(event.clientY / sizes.height - 0.5);
+});
+
+/**
+ * Base
+ */
+// Canvas
+const canvas = document.querySelector('canvas.webgl');
+
+// Scene
+const scene = new THREE.Scene();
+
+// Object
+const mesh = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1, 5, 5, 5),
+  new THREE.MeshBasicMaterial({ color: 'purple' })
+);
+scene.add(mesh);
+
 // Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+// fov vertical field of view
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+// const aspectRatio = sizes.width / sizes.height
+// const camera = new THREE.OrthographicCamera(
+//   -1 * aspectRatio,
+//   1 * aspectRatio,
+//   1,
+//   -1,
+//   0.1,
+//   100
+// )
+
+// camera.position.x = 2
+// camera.position.y = 2
 camera.position.z = 3;
+camera.lookAt(mesh.position);
 scene.add(camera);
+
+// Controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+// controls.target.y = 1
+// controls.update()
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
 });
 renderer.setSize(sizes.width, sizes.height);
-renderer.render(scene, camera);
 
-let time = new Date();
+// Animate
 const clock = new THREE.Clock();
 
-// gsap.to(mesh.position, { duration: 1, delay: 2, x: 2 });
-
 const tick = () => {
-  // This is for animating an object regardless of Frame rate of your PC.
-  const currentTime = Date.now();
-  const deltaTime = currentTime - time;
-  time = currentTime;
-
-  // Another Way to animate an object regardless of Frame rate of your PC.
-  // Clock
   const elapsedTime = clock.getElapsedTime();
 
-  // mesh.position.x += 0.01 * deltaTime;
-  // mesh.rotation.y += 0.001 * deltaTime;
+  // Update objects
+  //   mesh.rotation.y = elapsedTime
 
-  mesh.rotation.y = elapsedTime;
-  mesh.rotation.x = elapsedTime;
-  mesh.rotation.z = elapsedTime;
+  // Update camera
+  camera.position.x = cursor.x * 10;
+  camera.position.y = cursor.y * 10;
+  console.log({ cursor });
 
-  mesh.position.x = Math.sin(elapsedTime);
-  mesh.position.y = Math.cos(elapsedTime);
+  // camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3
+  // camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3
+  // camera.position.y = cursor.y * 5
+  //   camera.lookAt(mesh.position)
 
-  // camera.position.x = Math.sin(elapsedTime);
-  // camera.position.y = Math.cos(elapsedTime);
-  // camera.lookAt(mesh.position);
+  // Update controls
+  controls.update(); // needed for controls damping
 
+  // Render
   renderer.render(scene, camera);
+
+  // Call tick again on the next frame
   window.requestAnimationFrame(tick);
 };
 
