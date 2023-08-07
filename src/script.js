e3ebba5333d9ded14a4671960eb779f567775e49
aman-ur-rehman-sandhu <engr.amansandhu@outlook.com>
2023-08-07 05:48:59 +0500

    Creating Material and Apllying Force

--- src/script.js
+++ src/script.js
@@ -26,12 +26,12 @@ const textureLoader = new THREE.TextureLoader();
 const cubeTextureLoader = new THREE.CubeTextureLoader();
 
 const environmentMapTexture = cubeTextureLoader.load([
-  '/textures/environmentMaps/0/px.png',
-  '/textures/environmentMaps/0/nx.png',
-  '/textures/environmentMaps/0/py.png',
-  '/textures/environmentMaps/0/ny.png',
-  '/textures/environmentMaps/0/pz.png',
-  '/textures/environmentMaps/0/nz.png',
+  '/textures/environmentMaps/5/px.png',
+  '/textures/environmentMaps/5/nx.png',
+  '/textures/environmentMaps/5/py.png',
+  '/textures/environmentMaps/5/ny.png',
+  '/textures/environmentMaps/5/pz.png',
+  '/textures/environmentMaps/5/nz.png',
 ]);
 
 /**
@@ -41,20 +41,38 @@ const environmentMapTexture = cubeTextureLoader.load([
 const world = new CANNON.World();
 world.gravity.set(0, -9.82, 0);
 
+// Materials
+const defaultMaterial = new CANNON.Material('default');
+const defaultContactMaterial = new CANNON.ContactMaterial(
+  defaultMaterial,
+  defaultMaterial,
+  {
+    friction: 0.1,
+    restitution: 0.7,
+  }
+);
+
+world.addContactMaterial(defaultContactMaterial);
+world.defaultContactMaterial = defaultContactMaterial;
 // Sphere
 const sphereShape = new CANNON.Sphere(0.5);
 const sphereBody = new CANNON.Body({
   mass: 1,
   position: new CANNON.Vec3(0, 3, 0),
   shape: sphereShape,
+  // material: defaultMaterial,
 });
-
+sphereBody.applyLocalForce(
+  new CANNON.Vec3(150, 0, 0),
+  new CANNON.Vec3(0, 0, 0)
+);
 world.addBody(sphereBody);
 
 //  Floor
 const floorShape = new CANNON.Plane();
 const floorBody = new CANNON.Body();
 floorBody.addShape(floorShape);
+// floorBody.material = defaultMaterial;
 floorBody.quaternion.setFromAxisAngle(new CANNON.Vec3(-1, 0, 0), Math.PI * 0.5);
 world.addBody(floorBody);
 /**
@@ -170,6 +188,7 @@ const tick = () => {
   oldElapsedTime = elapsedTime;
 
   // Update Physics World
+  sphereBody.applyForce(new CANNON.Vec3(-0.5, 0, 0), sphereBody.position);
   world.step(1 / 60, deltaTime, 3);
 
   sphere.position.copy(sphereBody.position);
