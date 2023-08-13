6163a340356be39090fac697b6005def2aa095d1
aman-ur-rehman-sandhu <engr.amansandhu@outlook.com>
2023-08-13 09:31:28 +0500

    Physics With Cannon.js

--- src/script.js
+++ src/script.js
@@ -28,8 +28,21 @@ debugObject.createBox = () => {
   });
 };
 
+debugObject.reset = () => {
+  for (const object of objectsToUpdate) {
+    // Remove Body
+    object.body.removeEventListener('collide', playHitSound);
+    world.removeBody(object.body);
+
+    // Remove Mesh
+    scene.remove(object.mesh);
+  }
+  objectsToUpdate.splice(0, objectsToUpdate.length);
+};
+
 gui.add(debugObject, 'createSphere');
 gui.add(debugObject, 'createBox');
+gui.add(debugObject, 'reset');
 
 /**
  * Base
