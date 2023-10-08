edcc53d2d7dd3c015f6146a23c9c69691419707b
aman-ur-rehman-sandhu <engr.amansandhu@outlook.com>
2023-10-09 01:51:50 +0500

    Tone Mapping

--- src/script.js
+++ src/script.js
@@ -110,9 +110,22 @@ controls.enableDamping = true;
  */
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
+  antialias: true,
 });
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+// Tone mapping
+renderer.toneMapping = THREE.ReinhardToneMapping;
+renderer.toneMappingExposure = 3;
+gui.add(renderer, 'toneMapping', {
+  No: THREE.NoToneMapping,
+  Linear: THREE.LinearToneMapping,
+  Reinhard: THREE.ReinhardToneMapping,
+  Cineon: THREE.CineonToneMapping,
+  ACESFilmic: THREE.ACESFilmicToneMapping,
+});
+gui.add(renderer, 'toneMappingExposure').min(0).max(10).step(0.001);
 
 /**
  * Animate
