5da1990a4daadf32bed2a40b93b6b30749fafe8a
aman-ur-rehman-sandhu <engr.amansandhu@outlook.com>
2023-10-22 16:11:28 +0500

    Uniform

--- src/script.js
+++ src/script.js
@@ -42,10 +42,30 @@ const material = new THREE.RawShaderMaterial({
   vertexShader: testVertexShader,
   fragmentShader: testFragmentShader,
   transparent: true,
+  uniforms: {
+    uFrequency: { value: new THREE.Vector2(10, 5) },
+    uTime: { value: 0 },
+    uColor: { value: new THREE.Color('orange') },
+  },
 });
+
+// GUI
+gui
+  .add(material.uniforms.uFrequency.value, 'x')
+  .min(0)
+  .max(20)
+  .step(0.01)
+  .name('frequencyX');
+gui
+  .add(material.uniforms.uFrequency.value, 'y')
+  .min(0)
+  .max(20)
+  .step(0.01)
+  .name('frequencyY');
 
 // Mesh
 const mesh = new THREE.Mesh(geometry, material);
+mesh.scale.y = 2 / 3;
 scene.add(mesh);
 
 /**
@@ -103,6 +123,9 @@ const clock = new THREE.Clock();
 
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
+
+  // Update material
+  material.uniforms.uTime.value = elapsedTime;
 
   // Update controls
   controls.update();
