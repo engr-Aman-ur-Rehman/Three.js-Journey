7b56552c0faef8e849c1bf6bac8d91424bba5d91
aman-ur-rehman-sandhu <engr.amansandhu@outlook.com>
2023-10-22 15:49:34 +0500

    Fragment vertex with varying

--- src/shaders/test/vertex.glsl
+++ src/shaders/test/vertex.glsl
@@ -4,7 +4,20 @@
 
         attribute vec3 position;
 
-        void main()
-        {
-            gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
-        }
+        attribute float aRandom;
+
+        varying float vRandom;
+
+    void main()
+{
+    vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+    // modelPosition.z += sin(modelPosition.x * 10.0) * 0.1;
+     modelPosition.z += aRandom * 0.1;
+
+    vec4 viewPosition = viewMatrix * modelPosition;
+    vec4 projectedPosition = projectionMatrix * viewPosition;
+    
+    gl_Position = projectedPosition;
+
+    vRandom = aRandom;
+}
