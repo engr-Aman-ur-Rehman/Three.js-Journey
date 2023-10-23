52bc50f8a2f07c931ecd1e0b4bf5d818f496a1aa
aman-ur-rehman-sandhu <engr.amansandhu@outlook.com>
2023-10-23 23:32:25 +0500

    Starter Code

--- src/shaders/test/vertex.glsl
+++ src/shaders/test/vertex.glsl
@@ -1,43 +1,4 @@
- uniform mat4 projectionMatrix;
-        uniform mat4 viewMatrix;
-        uniform mat4 modelMatrix;
-        uniform vec2 uFrequency;
-        uniform float uTime;
-        uniform vec3 uColor;
-
-        attribute vec3 position;
-
-        attribute float aRandom;
-        // varying float vRandom;
-
-        attribute vec2 uv;
-
-        varying vec2 vUv;
-
-        varying float vElevation;
-
-        
-
-    void main()
+void main()
 {
-    vec4 modelPosition = modelMatrix * vec4(position, 1.0);
-
-    float elevation = sin(modelPosition.x * uFrequency.x - uTime) * 0.1;
-    elevation += sin(modelPosition.y * uFrequency.y - uTime) * 0.1;
-    
-    modelPosition.z += elevation;
-
-    // modelPosition.z += sin(modelPosition.x * uFrequency.x + uTime) * 0.1;
-    // modelPosition.z += sin(modelPosition.y * uFrequency.y + uTime) * 0.1;
-    //  modelPosition.z += aRandom * 0.1;
-
-    vec4 viewPosition = viewMatrix * modelPosition;
-    vec4 projectedPosition = projectionMatrix * viewPosition;
-    
-    gl_Position = projectedPosition;
-
-    // vRandom = aRandom;
-
-    vUv = uv;
-    vElevation = elevation;
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
 }
