a418358d7673e3e7d2fda553eb78f17cf37c7282
aman-ur-rehman-sandhu <engr.amansandhu@outlook.com>
2023-10-27 18:35:47 +0500

    small more detailed waves

--- src/script.js
+++ src/script.js
@@ -24,7 +24,7 @@ const scene = new THREE.Scene();
  * Water
  */
 // Geometry
-const waterGeometry = new THREE.PlaneGeometry(2, 2, 128, 128);
+const waterGeometry = new THREE.PlaneGeometry(2, 2, 256, 256);
 
 // Colors
 debugObject.depthColor = '#186691';
@@ -44,6 +44,11 @@ const waterMaterial = new THREE.ShaderMaterial({
     uSurfaceColor: { value: new THREE.Color(debugObject.surfaceColor) },
     uColorOffset: { value: 0.08 },
     uColorMultiplier: { value: 5 },
+
+    uSmallWavesElevation: { value: 0.15 },
+    uSmallWavesFrequency: { value: 3 },
+    uSmallWavesSpeed: { value: 0.2 },
+    uSmallIterations: { value: 4 },
   },
 });
 
@@ -92,6 +97,30 @@ gui
   .step(0.001)
   .name('uColorMultiplier');
 
+gui
+  .add(waterMaterial.uniforms.uSmallWavesElevation, 'value')
+  .min(0)
+  .max(1)
+  .step(0.001)
+  .name('uSmallWavesElevation');
+gui
+  .add(waterMaterial.uniforms.uSmallWavesFrequency, 'value')
+  .min(0)
+  .max(30)
+  .step(0.001)
+  .name('uSmallWavesFrequency');
+gui
+  .add(waterMaterial.uniforms.uSmallWavesSpeed, 'value')
+  .min(0)
+  .max(4)
+  .step(0.001)
+  .name('uSmallWavesSpeed');
+gui
+  .add(waterMaterial.uniforms.uSmallIterations, 'value')
+  .min(0)
+  .max(5)
+  .step(1)
+  .name('uSmallIterations');
 // Mesh
 const water = new THREE.Mesh(waterGeometry, waterMaterial);
 water.rotation.x = -Math.PI * 0.5;
