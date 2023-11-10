0833e727f507304ec8d67a33afa52e1049b4605a
aman-ur-rehman-sandhu <engr.amansandhu@outlook.com>
2023-11-11 01:09:45 +0500

    fixing the normals

--- src/script.js
+++ src/script.js
@@ -93,12 +93,20 @@ material.onBeforeCompile = (shader) => {
   `
     )),
     (shader.vertexShader = shader.vertexShader.replace(
+      '#include <beginnormal_vertex>',
+      `
+          #include <beginnormal_vertex>
+
+          float angle = (position.y + 4.0) * sin(uTime) * 0.9;
+          mat2 rotateMatrix = get2dRotateMatrix(angle);
+
+          objectNormal.xz = rotateMatrix * objectNormal.xz;
+      `
+    )),
+    (shader.vertexShader = shader.vertexShader.replace(
       '#include <begin_vertex>',
       `
       #include <begin_vertex>
-
-      float angle = (position.y + uTime) * 0.9;
-      mat2 rotateMatrix = get2dRotateMatrix(angle);
 
       transformed.xz = rotateMatrix * transformed.xz;
   `
