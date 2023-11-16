a44aeda4608278da3ed9ecd3a7581999f1c3884f
aman-ur-rehman-sandhu <engr.amansandhu@outlook.com>
2023-11-17 02:28:39 +0500

    Creating custom tint pass

--- src/script.js
+++ src/script.js
@@ -166,10 +166,11 @@ effectComposer.addPass(dotScreenPass);
 
 const glitchPass = new GlitchPass();
 glitchPass.goWild = false;
-glitchPass.enabled = true;
+glitchPass.enabled = false;
 effectComposer.addPass(glitchPass);
 
 const rgbShiftPass = new ShaderPass(RGBShiftShader);
+rgbShiftPass.enabled = false;
 effectComposer.addPass(rgbShiftPass);
 
 const unrealBloomPass = new UnrealBloomPass();
@@ -182,6 +183,60 @@ gui.add(unrealBloomPass, 'enabled');
 gui.add(unrealBloomPass, 'strength').min(0).max(2).step(0.001);
 gui.add(unrealBloomPass, 'radius').min(0).max(2).step(0.001);
 gui.add(unrealBloomPass, 'threshold').min(0).max(1).step(0.001);
+
+const TintShader = {
+  uniforms: {
+    tDiffuse: { value: null },
+    uTint: { value: null },
+  },
+  vertexShader: `
+        varying vec2 vUv;
+
+        void main()
+        {
+            gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+
+            vUv = uv;
+        }
+    `,
+  fragmentShader: `
+    uniform sampler2D tDiffuse;
+    uniform vec3 uTint;
+
+    varying vec2 vUv;
+
+    void main()
+    {
+        vec4 color = texture2D(tDiffuse, vUv);
+        color.rgb += uTint;
+
+        gl_FragColor = color;
+    }
+`,
+};
+
+const tintPass = new ShaderPass(TintShader);
+tintPass.material.uniforms.uTint.value = new THREE.Vector3();
+effectComposer.addPass(tintPass);
+
+gui
+  .add(tintPass.material.uniforms.uTint.value, 'x')
+  .min(-1)
+  .max(1)
+  .step(0.001)
+  .name('red');
+gui
+  .add(tintPass.material.uniforms.uTint.value, 'y')
+  .min(-1)
+  .max(1)
+  .step(0.001)
+  .name('green');
+gui
+  .add(tintPass.material.uniforms.uTint.value, 'z')
+  .min(-1)
+  .max(1)
+  .step(0.001)
+  .name('blue');
 
 // const gammaCorrectionPass = new ShaderPass(GammaCorrectionShader);
 // effectComposer.addPass(gammaCorrectionPass);
