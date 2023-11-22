b49e1ecbf0af2b9fd84c407df339b506bcf40051
aman-ur-rehman-sandhu <engr.amansandhu@outlook.com>
2023-11-22 07:22:37 +0500

    Loading Bar using HTML

--- src/script.js
+++ src/script.js
@@ -6,15 +6,27 @@ import { gsap } from 'gsap';
 /**
  * Loaders
  */
+const loadingBarElement = document.querySelector('.loading-bar');
+
 const loadingManager = new THREE.LoadingManager(
   // Loaded
   () => {
-    gsap.to(overlayMaterial.uniforms.uAlpha, { duration: 3, value: 0 });
+    window.setTimeout(() => {
+      gsap.to(overlayMaterial.uniforms.uAlpha, {
+        duration: 3,
+        value: 0,
+        delay: 1,
+      });
+
+      loadingBarElement.classList.add('ended');
+      loadingBarElement.style.transform = '';
+    }, 500);
   },
 
   // Progress
-  () => {
-    console.log('progress');
+  (itemUrl, itemsLoaded, itemsTotal) => {
+    const progressRatio = itemsLoaded / itemsTotal;
+    loadingBarElement.style.transform = `scaleX(${progressRatio})`;
   }
 );
 const gltfLoader = new GLTFLoader(loadingManager);
