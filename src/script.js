eae52fd9d7b7114a55f8fe8942452c886f822210
aman-ur-rehman-sandhu <engr.amansandhu@outlook.com>
2023-11-29 16:55:58 +0500

    Portal Custom Shaders

--- src/script.js
+++ src/script.js
@@ -55,6 +55,10 @@ const poleLightMaterial = new THREE.MeshBasicMaterial({ color: 0xffffe5 });
 
 // Portal light material
 const portalLightMaterial = new THREE.ShaderMaterial({
+  uniforms:
+    {
+        uTime: { value: 0 }
+    },
   vertexShader: portalVertexShader,
   fragmentShader: portalFragmentShader
 })
@@ -199,7 +203,7 @@ const tick = () => {
 
   // Update materials
   firefliesMaterial.uniforms.uTime.value = elapsedTime
-
+  portalLightMaterial.uniforms.uTime.value = elapsedTime
   // Update controls
   controls.update();
 
--- src/shaders/portal/vertex.glsl
+++ src/shaders/portal/vertex.glsl
@@ -1,5 +1,6 @@
 varying vec2 vUv;
 
+
 void main()
 {
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
