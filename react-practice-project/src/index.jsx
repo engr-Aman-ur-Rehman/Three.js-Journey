a4b656f04c1277d4906dde535c5aedb5bf34119b
aman-ur-rehman-sandhu <engr.amansandhu@outlook.com>
2023-12-08 18:19:19 +0500

    index

--- react-practice-project/src/index.jsx
+++ react-practice-project/src/index.jsx
@@ -1,8 +1,8 @@
-// import { createRoot } from 'react-dom/client'
-// import App from './App.jsx'
+import './style.css'
+import { createRoot } from 'react-dom/client'
 
-// const root = createRoot(document.querySelector('#root'))
+const root = createRoot(document.querySelector('#root'))
 
-// root.render(
-    
-// )
+root.render(
+    <h1>Hello React</h1>
+)
