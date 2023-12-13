2607042bb4cf0d82f247cd06be2f80376b699079
aman-ur-rehman-sandhu <engr.amansandhu@outlook.com>
2023-12-13 21:20:00 +0500

    handling meshes

--- react-three-fiber/src/Experience.jsx
+++ react-three-fiber/src/Experience.jsx
@@ -1,9 +1,9 @@
 export default function Experience()
 {
     return <>
-        <mesh>
-            <torusKnotGeometry />
-            <meshNormalMaterial />
-        </mesh>
+    <mesh rotation-y={ Math.PI * 0.25 } position-x={ 2} scale={ 1.5 }>
+    <sphereGeometry args={ [ 1.5, 32, 32 ] } />
+    <meshBasicMaterial color="mediumpurple" wireframe={ false } />
+    </mesh>
     </>
 }
