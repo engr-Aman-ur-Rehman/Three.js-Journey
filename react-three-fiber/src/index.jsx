4070fa71b6cad9daae7ac91dbe8bb90aade15536
aman-ur-rehman-sandhu <engr.amansandhu@outlook.com>
2023-12-15 02:22:13 +0500

    Canvas Settings

--- react-three-fiber/src/index.jsx
+++ react-three-fiber/src/index.jsx
@@ -1,14 +1,31 @@
 import './style.css'
 import ReactDOM from 'react-dom/client'
 import { Canvas } from '@react-three/fiber'
-
+import * as THREE from 'three'
 import Experience from './Experience.jsx'
+
 const root = ReactDOM.createRoot(document.querySelector('#root'))
 
+const cameraSettings = {
+    fov: 45,
+    // zoom: 100,
+    near: 0.1,
+    far: 200,
+    position: [ 3, 2, 6 ]
+}
+
 root.render(
-
-    <Canvas>
-    <Experience />
-</Canvas>
-
+    <Canvas
+        // flat
+        // dpr={ [ 1, 2 ] }
+        gl={ {
+            antialias: true,
+            toneMapping: THREE.CineonToneMapping,
+            // outputColorSpace: THREE.LinearSRGBColorSpace
+        } }
+        // orthographic
+        camera={ cameraSettings }
+    >
+        <Experience />
+    </Canvas>
 )
