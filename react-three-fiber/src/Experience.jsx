031ad4699b83127e22fa5d39c31eb7dc6822f01f
aman-ur-rehman-sandhu <engr.amansandhu@outlook.com>
2023-12-15 23:04:03 +0500

    PivotControls

--- react-three-fiber/src/Experience.jsx
+++ react-three-fiber/src/Experience.jsx
@@ -1,4 +1,4 @@
-import { TransformControls, OrbitControls } from '@react-three/drei'
+import { PivotControls, TransformControls, OrbitControls } from '@react-three/drei'
 import { useRef } from 'react'
 
 export default function Experience()
@@ -13,10 +13,17 @@ export default function Experience()
         <directionalLight position={ [ 1, 2, 3 ] } intensity={ 4.5 } />
         <ambientLight intensity={ 1.5 } />
 
-        <mesh position-x={ - 2 }>
-            <sphereGeometry />
-            <meshStandardMaterial color="orange" />
-        </mesh>
+        <PivotControls anchor={ [ 0, 0, 0 ] }
+    depthTest={ false }
+    lineWidth={ 4 }
+    axisColors={ [ '#9381ff', '#ff4d6d', '#7ae582' ] }
+    scale={ 2 }
+    >
+            <mesh position-x={ - 2 }>
+                <sphereGeometry />
+                <meshStandardMaterial color="orange" />
+            </mesh>
+        </PivotControls>
 
         <mesh ref={ cube } position-x={ 2 } scale={ 1.5 }>
             <boxGeometry />
