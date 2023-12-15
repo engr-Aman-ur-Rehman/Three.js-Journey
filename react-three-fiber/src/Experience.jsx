abd0bc4bfaf0f7f1b519cdaec8b51216a275805e
aman-ur-rehman-sandhu <engr.amansandhu@outlook.com>
2023-12-15 22:45:36 +0500

    orbitControls and TransformControl in Drei

--- react-three-fiber/src/Experience.jsx
+++ react-three-fiber/src/Experience.jsx
@@ -1,16 +1,15 @@
-import { useThree, extend } from '@react-three/fiber'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-
-extend({ OrbitControls })
+import { TransformControls, OrbitControls } from '@react-three/drei'
+import { useRef } from 'react'
 
 export default function Experience()
 {
-    const { camera, gl } = useThree()
+    const cube = useRef()
 
     return <>
 
-        <orbitControls args={ [ camera, gl.domElement ] } />
+        <OrbitControls makeDefault />
 
+        
         <directionalLight position={ [ 1, 2, 3 ] } intensity={ 4.5 } />
         <ambientLight intensity={ 1.5 } />
 
@@ -19,10 +18,12 @@ export default function Experience()
             <meshStandardMaterial color="orange" />
         </mesh>
 
-        <mesh position-x={ 2 } scale={ 1.5 }>
+        <mesh ref={ cube } position-x={ 2 } scale={ 1.5 }>
             <boxGeometry />
             <meshStandardMaterial color="mediumpurple" />
         </mesh>
+
+        <TransformControls object={ cube }  mode="translate"/>
 
         <mesh position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
             <planeGeometry />
