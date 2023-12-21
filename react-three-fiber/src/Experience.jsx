965e24cec99f3e810ad4524a29a7d7aabe7ce62f
aman-ur-rehman-sandhu <engr.amansandhu@outlook.com>
2023-12-21 06:37:14 +0500

    ContactShadow

--- react-three-fiber/src/Experience.jsx
+++ react-three-fiber/src/Experience.jsx
@@ -1,8 +1,9 @@
 import { useFrame } from '@react-three/fiber'
-import {  RandomizedLight, AccumulativeShadows, SoftShadows, BakeShadows, useHelper, OrbitControls } from '@react-three/drei'
+import { ContactShadows, RandomizedLight, AccumulativeShadows, SoftShadows, BakeShadows, useHelper, OrbitControls } from '@react-three/drei'
 import { useRef } from 'react'
 import { Perf } from 'r3f-perf'
 import * as THREE from 'three'
+import { useControls } from 'leva'
 
 export default function Experience()
 {
@@ -19,6 +20,12 @@ export default function Experience()
         cube.current.rotation.y += delta * 0.2
     })
 
+    const { color, opacity, blur } = useControls('contact shadows', {
+        color: '#1d8f75',
+        opacity: { value: 0.4, min: 0, max: 1 },
+        blur: { value: 2.8, min: 0, max: 10 },
+    })
+
     return <>
 
         <color args={ [ 'ivory' ] } attach="background" />
@@ -31,7 +38,7 @@ export default function Experience()
 
         <OrbitControls makeDefault />
         
-        <AccumulativeShadows 
+        {/* <AccumulativeShadows 
         position={ [ 0, - 0.99, 0 ] }
         scale={ 10 }
         color="#316d39"
@@ -48,7 +55,18 @@ export default function Experience()
         position={ [ 1, 2, 3 ] }
         bias={ 0.001 }
         />
-        </AccumulativeShadows>
+        </AccumulativeShadows> */}
+
+        <ContactShadows 
+        position={ [ 0, - 0.99, 0 ] }
+        scale={ 10 }
+        resolution={ 512 }
+        far={ 5 }
+        color={ color }
+        opacity={ opacity }
+        blur={ blur }
+        frames={ 1 }
+        />  
 
         <directionalLight 
         ref={ directionalLight } 
