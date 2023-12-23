1aba754f4e3c8bb3ad54929e183241b934e6e3d1
aman-ur-rehman-sandhu <engr.amansandhu@outlook.com>
2023-12-23 08:45:56 +0500

    convert gltf models to react components

--- react-three-fiber/src/Experience.jsx
+++ react-three-fiber/src/Experience.jsx
@@ -4,6 +4,7 @@ import { Suspense } from 'react'
 
 import Model from './Model.jsx'
 import Placeholder from './Placeholder.jsx'
+import Hamburger from './Hamburger.jsx'
 
 export default function Experience()
 {
@@ -14,7 +15,7 @@ export default function Experience()
 
         <OrbitControls makeDefault />
 
-        <directionalLight castShadow position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
+        <directionalLight castShadow position={ [ 1, 2, 3 ] } intensity={ 1.5 } shadow-normalBias={ 0.04 }/>
         <ambientLight intensity={ 0.5 } />
 
         <mesh receiveShadow position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
@@ -23,7 +24,7 @@ export default function Experience()
         </mesh>
 
         <Suspense fallback={ <Placeholder position-y={ 0.5 } scale={ [ 2, 3, 2 ] }/> }>
-         <Model />
+        <Hamburger scale={ 0.35 } />
         </Suspense>
     </>
 }
