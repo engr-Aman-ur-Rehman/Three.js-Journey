6e967632fc6e55fd9ed930223e5abf99b9391b7f
aman-ur-rehman-sandhu <engr.amansandhu@outlook.com>
2024-01-01 18:26:14 +0500

    Animation and Controls

--- react-three-fiber/src/Experience.jsx
+++ react-three-fiber/src/Experience.jsx
@@ -1,4 +1,4 @@
-import {Environment, useGLTF, OrbitControls } from '@react-three/drei'
+import {PresentationControls, Float, Environment, useGLTF, OrbitControls } from '@react-three/drei'
 
 export default function Experience()
 {
@@ -9,14 +9,29 @@ export default function Experience()
         
         <color args={ [ '#241a1a' ] } attach="background" />
 
-        <OrbitControls makeDefault />
+        {/* <OrbitControls makeDefault /> */}
         
         {/* <mesh>
             <boxGeometry />
             <meshNormalMaterial />
         </mesh> */}
 
-        <primitive object={ computer.scene } />
+
+        <PresentationControls 
+        global
+        rotation={ [ 0.13, 0.1, 0 ] }
+        polar={ [ - 0.4, 0.2 ] }
+        azimuth={ [ - 1, 0.75 ] }
+        config={ { mass: 2, tension: 400 } }
+        snap={ { mass: 4, tension: 400 } }
+        >
+            <Float rotationIntensity={ 0.4 } >
+                <primitive
+                    object={ computer.scene }
+                    position-y={ - 1.2 }
+                />
+            </Float>
+        </PresentationControls>
 
     </>
 }
