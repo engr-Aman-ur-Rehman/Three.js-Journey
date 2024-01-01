07ad10f686f543886dfa6fdee9cbe1f7c0909fce
aman-ur-rehman-sandhu <engr.amansandhu@outlook.com>
2024-01-01 17:59:12 +0500

    Loading Laptop model and environment

--- react-three-fiber/src/Experience.jsx
+++ react-three-fiber/src/Experience.jsx
@@ -1,15 +1,22 @@
-import { OrbitControls } from '@react-three/drei'
+import {Environment, useGLTF, OrbitControls } from '@react-three/drei'
 
 export default function Experience()
 {
+    const computer = useGLTF('https://threejs-journey.com/resources/models/macbook_model.gltf')
+
     return <>
+        <Environment preset="city" />
+        
+        <color args={ [ '#241a1a' ] } attach="background" />
 
         <OrbitControls makeDefault />
         
-        <mesh>
+        {/* <mesh>
             <boxGeometry />
             <meshNormalMaterial />
-        </mesh>
+        </mesh> */}
+
+        <primitive object={ computer.scene } />
 
     </>
 }
