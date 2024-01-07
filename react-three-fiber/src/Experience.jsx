045209e26df050972b853327779b101bc5cd2a7c
aman-ur-rehman-sandhu <engr.amansandhu@outlook.com>
2024-01-07 14:55:14 +0500

    Cuboid, ball, hull, & Trimesh Collider

--- react-three-fiber/src/Experience.jsx
+++ react-three-fiber/src/Experience.jsx
@@ -13,18 +13,32 @@ export default function Experience()
         <directionalLight castShadow position={ [ 1, 2, 3 ] } intensity={ 4.5 } />
         <ambientLight intensity={ 1.5 } />
         
-        <Physics >
-            <RigidBody>
-                <mesh castShadow position={ [ - 2, 2, 0 ] }>
+        <Physics debug>
+
+            <RigidBody colliders="ball">
+                <mesh castShadow position={ [ 0, 4, 0 ] }>
                     <sphereGeometry />
                     <meshStandardMaterial color="orange" />
                 </mesh>
             </RigidBody>
 
-            <mesh castShadow position={ [ 2, 2, 0 ] }>
-                <boxGeometry />
-                <meshStandardMaterial color="mediumpurple" />
-            </mesh>
+            {/* <RigidBody>
+                <mesh castShadow position={ [ 2, 2, 0 ] }>
+                    <boxGeometry args={ [ 3, 2, 1 ] } />
+                    <meshStandardMaterial color="mediumpurple" />
+                </mesh>
+                <mesh castShadow position={ [ 2, 2, 3 ] }>
+                    <boxGeometry args={ [ 1, 1, 1 ] } />
+                    <meshStandardMaterial color="mediumpurple" />
+                </mesh>
+            </RigidBody> */}
+
+            <RigidBody colliders="trimesh">
+                <mesh castShadow position={ [ 0, 1, 0 ] } rotation={ [ Math.PI * 0.5, 0, 0 ] }>
+                    <torusGeometry args={ [ 1, 0.5, 16, 32 ] } />
+                    <meshStandardMaterial color="mediumpurple" />
+                </mesh>
+            </RigidBody>
 
             <RigidBody type='fixed'>
                 <mesh receiveShadow position-y={ - 1.25 }>
