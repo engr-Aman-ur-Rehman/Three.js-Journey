e8e67daff5f23b1024cfe3066d56eeef7eb3ad24
aman-ur-rehman-sandhu <engr.amansandhu@outlook.com>
2024-01-09 18:21:34 +0500

    Walls

--- react-three-fiber/src/Level.jsx
+++ react-three-fiber/src/Level.jsx
@@ -1,5 +1,5 @@
 import * as THREE from 'three'
-import { RigidBody } from '@react-three/rapier'
+import {CuboidCollider, RigidBody } from '@react-three/rapier'
 import {useMemo, useState, useRef } from 'react'
 import { useFrame } from '@react-three/fiber'
 import { useGLTF } from '@react-three/drei'
@@ -125,6 +125,39 @@ export function BlockEnd({ position = [ 0, 0, 0 ] })
     </group>
 }
 
+function Bounds({ length = 1 })
+{
+    return <>
+        <RigidBody type="fixed" restitution={ 0.2 } friction={ 0 }>
+            <mesh
+                position={ [ 2.15, 0.75, - (length * 2) + 2 ] }
+                geometry={ boxGeometry }
+                material={ wallMaterial }
+                scale={ [ 0.3, 1.5, 4 * length ] }
+                castShadow
+            />
+            <mesh
+                position={ [ - 2.15, 0.75, - (length * 2) + 2 ] }
+                geometry={ boxGeometry }
+                material={ wallMaterial }
+                scale={ [ 0.3, 1.5, 4 * length ] }
+                receiveShadow
+            />
+            <mesh
+                    position={ [ 0, 0.75, - (length * 4) + 2] }
+                    geometry={ boxGeometry }
+                    material={ wallMaterial }
+                    scale={ [ 4, 1.5, 0.3 ] }
+                    receiveShadow
+            />
+             <CuboidCollider 
+             args={ [ 1, 1, 1 ] } 
+             position={ [ 0, -0.1, - (length * 2) + 2 ] }/>
+        </RigidBody>
+    </>
+}
+
+
 export function Level({ count = 5, types = [ BlockSpinner, BlockAxe, BlockLimbo ] })
 {
     const blocks = useMemo(() =>
@@ -146,5 +179,7 @@ export function Level({ count = 5, types = [ BlockSpinner, BlockAxe, BlockLimbo 
         { blocks.map((Block, index) => <Block key={ index } position={ [ 0, 0, - (index + 1) * 4 ] } />) } 
         
         <BlockEnd position={ [ 0, 0, - (count + 1) * 4 ] } />
+        
+        <Bounds length={ count + 2 }/>
      </>
 }
