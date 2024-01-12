896c3e19304f4868a223bfe5fa21d8d3d4c17896
aman-ur-rehman-sandhu <engr.amansandhu@outlook.com>
2024-01-12 21:48:13 +0500

    Game mechanics

--- react-three-fiber/src/Player.jsx
+++ react-three-fiber/src/Player.jsx
@@ -3,6 +3,7 @@ import { useFrame } from '@react-three/fiber'
 import { useKeyboardControls } from '@react-three/drei'
 import { useState, useEffect, useRef } from 'react'
 import * as THREE from 'three'
+import useGame from './stores/useGame.jsx'
 
 export default function Player()
 {
@@ -17,6 +18,11 @@ export default function Player()
     const [ smoothedCameraPosition ] = useState(() => new THREE.Vector3((10, 10, 10)))
     const [ smoothedCameraTarget ] = useState(() => new THREE.Vector3())
 
+    const start = useGame((state) => state.start)
+    const end = useGame((state) => state.end)
+    const restart = useGame((state) => state.restart)
+    const blocksCount = useGame((state) => state.blocksCount)
+
     const jump = () =>
     {
         const origin = body.current.translation()
@@ -28,8 +34,25 @@ export default function Player()
             body.current.applyImpulse({ x: 0, y: 0.5, z: 0 })
     }
 
+    const reset = () =>
+    {
+        body.current.setTranslation({ x: 0, y: 1, z: 0 })
+        body.current.setLinvel({ x: 0, y: 0, z: 0 })
+        body.current.setAngvel({ x: 0, y: 0, z: 0 })
+    }
+
     useEffect(() =>
     {
+        const unsubscribeReset = useGame.subscribe(
+            (state) => state.phase,
+            (value) =>
+            {
+                if(value === 'ready')
+                    reset()
+            }
+            
+        )
+
         const unsubscribeJump = subscribeKeys(
             (state) => state.jump,
             (value) =>
@@ -38,10 +61,19 @@ export default function Player()
                     jump()
             }
         )
-
+        const unsubscribeAny = subscribeKeys(
+            () =>
+            {
+                start()
+            }
+        )
+        
         return () =>
     {
+        unsubscribeReset()
         unsubscribeJump()
+        unsubscribeAny()
+        
     }
     }, [])
 
@@ -102,6 +134,16 @@ export default function Player()
 
         state.camera.position.copy(smoothedCameraPosition)
         state.camera.lookAt(smoothedCameraTarget)
+
+        /**
+        * Phases
+        */
+        if(bodyPosition.z < - (blocksCount * 4 + 2))
+            end()
+
+        if(bodyPosition.y < - 4)
+            restart()
+
     })
 
     return <>
