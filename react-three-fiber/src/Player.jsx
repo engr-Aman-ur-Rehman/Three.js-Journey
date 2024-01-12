5e5bba88d8fc8596ab80518aa5d003754c960fbb
aman-ur-rehman-sandhu <engr.amansandhu@outlook.com>
2024-01-12 20:42:59 +0500

    Camera animation

--- react-three-fiber/src/Player.jsx
+++ react-three-fiber/src/Player.jsx
@@ -1,15 +1,21 @@
 import { useRapier, RigidBody } from '@react-three/rapier'
 import { useFrame } from '@react-three/fiber'
 import { useKeyboardControls } from '@react-three/drei'
-import { useEffect, useRef } from 'react'
+import { useState, useEffect, useRef } from 'react'
+import * as THREE from 'three'
 
 export default function Player()
 {
+
+    
     const body = useRef()
 
     const [ subscribeKeys, getKeys ] = useKeyboardControls()
 
     const { rapier, world  } = useRapier()
+
+    const [ smoothedCameraPosition ] = useState(() => new THREE.Vector3((10, 10, 10)))
+    const [ smoothedCameraTarget ] = useState(() => new THREE.Vector3())
 
     const jump = () =>
     {
@@ -75,6 +81,27 @@ export default function Player()
 
         body.current.applyImpulse(impulse)
         body.current.applyTorqueImpulse(torque)
+
+
+        /**
+         * Camera
+         */
+
+        const bodyPosition = body.current.translation()
+        const cameraPosition = new THREE.Vector3()
+        cameraPosition.copy(bodyPosition)
+        cameraPosition.z += 2.25
+        cameraPosition.y += 0.65
+
+        const cameraTarget = new THREE.Vector3()
+        cameraTarget.copy(bodyPosition)
+        cameraTarget.y += 0.25
+
+        smoothedCameraPosition.lerp(cameraPosition, 5 * delta)
+        smoothedCameraTarget.lerp(cameraTarget, 5 * delta)
+
+        state.camera.position.copy(smoothedCameraPosition)
+        state.camera.lookAt(smoothedCameraTarget)
     })
 
     return <>
